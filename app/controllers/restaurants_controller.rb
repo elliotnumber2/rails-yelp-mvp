48c42a7ea278f88383c8d633429ed742278b14bb
Elliot Jensen <elliotnumber2@gmail.com>
2020-05-20 16:12:07 +0200

    Mostly finish app

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,29 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(resty_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def resty_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+peking = { name: 'Peking', address: 'Bellingham', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: 'London', category: 'italian' }
+blue_sea = { name: 'Blue Sea', address: 'Seattle', category: 'japanese' }
+le_coq = { name: 'Le Coq', address: 'France', category: 'french' }
+le_chouffe = { name: 'Le Chouffe', address: 'Belgium', category: 'belgian' }
+
+[ peking, pizza_east, blue_sea, le_coq, le_chouffe ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
