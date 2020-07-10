077b0e9ee37bb79cf3170983a6b399fd0e7125b1
Elliot Jensen <elliotnumber2@gmail.com>
2020-07-10 13:48:20 +0200

    Mess around with JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+const reviews = document.getElementById("reviews");
+
+console.log(reviews);
+
+// style
